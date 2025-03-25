d6a05eaa98feb3d14cd7806de9f5e78c167d18e3
Yazmin1606 <kh931286@gmail.com>
2025-03-24 23:55:57 -0600

    lógica de filtrado de usuarios

--- src/app/buscar/buscar.page.ts
+++ src/app/buscar/buscar.page.ts
@@ -6,14 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buscar.page.scss'],
   standalone: false
 })
-export class BuscarPage {
+export class BuscarPage implements OnInit {
   searchTerms: string = '';
 
 
-  constructor() { }
+  usuarios = [
+    { username: 'karol_y', followers: 1200, following: false },
+    { username: 'madai_loc', followers: 950, following: true },
+    { username: 'teoch_bill', followers: 470, following: false },
+  ];
 
-  filterItems() {
-    console.log('buscando:', this.searchTerms);
+  filteredUsers = [...this.usuarios]; 
+
+  constructor() {}
+
+  ngOnInit() {
+    this.filterItems(); 
   }
 
+  filterItems() {
+    const term = this.searchTerms.toLowerCase();
+    this.filteredUsers = this.usuarios.filter(user =>
+      user.username.toLowerCase().includes(term)
+    );
+  }
+
+  followUser(user: any) {
+    user.following = !user.following;
+  }
 }
