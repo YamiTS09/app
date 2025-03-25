2d885702c7335cc7a54af25bf7c04da8729345f0
ElAleBsS <20203i101079@utcv.edu.mx>
2025-03-24 22:35:53 -0600

    Ajuste de los botones y llamado del componente de hilos

--- src/app/components/usuario-perfil/usuario-perfil.component.ts
+++ src/app/components/usuario-perfil/usuario-perfil.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { IonAvatar, IonItem } from "@ionic/angular/standalone";
+import { NuevoHiloComponent } from "../nuevo-hilo/nuevo-hilo.component";
 
 @Component({
   selector: 'app-usuario-perfil',
   templateUrl: './usuario-perfil.component.html',
   styleUrls: ['./usuario-perfil.component.scss'],
-  imports: [IonicModule]	
+  imports: [IonicModule, NuevoHiloComponent]	
 })
 export class UsuarioPerfilComponent  implements OnInit {
 
