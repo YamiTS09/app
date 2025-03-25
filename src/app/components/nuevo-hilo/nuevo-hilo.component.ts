0dab972c6702dea01cb89fd15d4629993e71a272
YamiTS09 <20233L001040@utcv.edu.mx>
2025-03-24 22:35:50 -0600

    Implementación y funcionalidad para el icono de camara

--- src/app/components/nuevo-hilo/nuevo-hilo.component.ts
+++ src/app/components/nuevo-hilo/nuevo-hilo.component.ts
@@ -33,5 +33,20 @@ export class NuevoHiloComponent implements OnInit {
     }
   }
 
+  async abrirCamara() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Base64,
+        source: CameraSource.Camera  
+      });
+
+      this.image = 'data:image/jpeg;base64,' + image.base64String;
+    } catch (error) {
+      console.error('Error al tomar la foto:', error);
+    }
+  }
+
   ngOnInit() {}
 }
